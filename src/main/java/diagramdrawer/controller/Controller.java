cfacc18141cadf32c8751a8d7ec95afe7701396d
luke newton <lukeantonynewton@gmail.com>
2020-07-13 18:54:13 -0400

    add comments to controller

--- src/main/java/diagramdrawer/controller/Controller.java
+++ src/main/java/diagramdrawer/controller/Controller.java
@@ -58,8 +58,19 @@ public class Controller {
                 DEFAULT_TWO_SECTION_BOX_HEIGHT, DEFAULT_TWO_SECTION_BOX_WIDTH));
     }
 
+    /**
+     * sets a event handler for creating a new object on the given graphics context that is of type classBox,
+     * and has a specified height and width. The handler sets a click event on the canvas that draws the specified
+     * DrawableComponent at the location clicked, or cancels the operation if the click is a right click
+     *
+     * @param gc the graphics context to draw on
+     * @param classBox the class to draw on the canvas
+     * @param height the height of the new object to draw
+     * @param width the width of the new object ot draw
+     */
     private void setNewComponentListeners(GraphicsContext gc, Class<? extends DrawableComponent> classBox,
                                           int height, int width){
+        //add listener to draw the object on left click, and cancel the operation on right click
         canvas.setOnMouseClicked(clickEvent -> {
             if(clickEvent.getButton() == MouseButton.SECONDARY){
                 // a right click indicates cancelling the new component addition
@@ -73,6 +84,7 @@ public class Controller {
                 }
             }
         });
+        //add listener to draw preview on canvas whenever the mouse is moved to a new location
         canvas.setOnMouseMoved(moveEvent -> {
             try {
                 drawPreviewComponent(gc, classBox, moveEvent.getX(), moveEvent.getY(), height, width);
@@ -82,11 +94,24 @@ public class Controller {
         });
     }
 
+    /**used to reset the canvas mouse event handlers when they aree no longer needed*/
     private void cancelNewComponentListeners(){
         canvas.setOnMouseMoved(null);
         canvas.setOnMouseClicked(null);
     }
 
+    /**
+     * draws a DrawableComponent in black onto the canvas at the specified X and Y coordinates,
+     * with a specified height and width. The component is added to the master list of canvas contents
+     * and the mouse event handlers for drawing a new component are removed
+     *
+     * @param gc the graphics context of the canvas to draw onto
+     * @param classBox the type of DrawableComponent to draw
+     * @param clickX the X coordinate at the center of the object to draw
+     * @param clickY the Y coordinate at the center of the object to draw
+     * @param height the height of the object to draw
+     * @param width the width of the object to draw
+     */
     private void drawFinalComponent(GraphicsContext gc, Class<? extends DrawableComponent> classBox, double clickX, double clickY,
                                     int height, int width) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         cancelNewComponentListeners();
@@ -94,6 +119,18 @@ public class Controller {
         drawnComponents.add(newComponent);
     }
 
+    /**
+     * draws a DrawableComponent in light gray onto the canvas at the specified X and Y coordinates,
+     * with a specified height and width. This is intended as a preview of what the object will look
+     * like when clicked
+     *
+     * @param gc the graphics context of the canvas to draw onto
+     * @param classBox the type of DrawableComponent to draw
+     * @param clickX the X coordinate at the center of the object to draw
+     * @param clickY the Y coordinate at the center of the object to draw
+     * @param height the height of the object to draw
+     * @param width the width of the object to draw
+     */
     private void drawPreviewComponent(GraphicsContext gc, Class<? extends DrawableComponent> classBox, double clickX, double clickY,
                                       int height, int width) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         clearTempCanvasContents(gc);
