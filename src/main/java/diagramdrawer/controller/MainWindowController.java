c1905c797e4d9f50ef90f4f57afeb5c2057dc0b3
luke newton <lukeantonynewton@gmail.com>
2020-07-31 13:10:03 -0400

    fix bug that prevents deleting components after load

--- src/main/java/diagramdrawer/controller/MainWindowController.java
+++ src/main/java/diagramdrawer/controller/MainWindowController.java
@@ -2,6 +2,7 @@ package diagramdrawer.controller;
 
 
 import diagramdrawer.controller.canvasstate.AddComponentState;
+import diagramdrawer.controller.canvasstate.SelectComponentState;
 import diagramdrawer.model.drawablecomponent.boxcomponent.SingleSectionClassBox;
 import diagramdrawer.model.drawablecomponent.boxcomponent.ThreeSectionClassBox;
 import diagramdrawer.model.drawablecomponent.boxcomponent.TwoSectionClassBox;
@@ -96,7 +97,10 @@ public class MainWindowController {
     public void loadCanvasContents(){
         canvasContentManagementController.setDrawnComponents(
                 fileController.loadDrawnComponents(root.getScene().getWindow()));
+
+        //after loading the file, we need to refresh the canvas drawing and reset the keystroke handler
         canvasContentManagementController.getCanvasDrawController().redrawCanvas();
+        canvasContentManagementController.setCurrentCanvasState(new SelectComponentState(canvasContentManagementController));
     }
 
     /**handler for saving the current set of DrawableComponents on the canvas to a file*/
